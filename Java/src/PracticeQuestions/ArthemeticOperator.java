a3f7f41389a9c63d765cbe2fa192f23acc56206c
Shambhavi <Shambhavi@EKA-BLR-L0635.ekablr.com>
2026-02-25 22:21:55 +0530

    Updated India as per review

--- Java/src/PracticeQuestions/ArthemeticOperator.java
+++ Java/src/PracticeQuestions/ArthemeticOperator.java
@@ -12,6 +12,6 @@ public class ArthemeticOperator {
         System.out.println(p);
         System.out.println(a);
         System.out.println(b);
-        System.out.println("Hello india");
+        System.out.println("Hello India");
     }
 }
